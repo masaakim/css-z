a2b46165a7eb8bb6f54f1285f27b2c503cdba0c9
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-08 02:04:29 +0900

    write test

---
+++ test/test.js
@@ -0,0 +1,33 @@
+
+var z_index = require('../');
+var test = require('colored-tape');
+
+var css = ".z1{z-index:10;}.z2{z-index:20;}.z3{z-index:-3;}.z4{z-index:0;}.z5{z-index:9999;}"
+
+test('z_index.num', function(t) {
+  var result = 5;
+  var expected = z_index.num(css);
+
+  t.equal(result, expected);
+
+  t.enc();
+});
+
+test('z_index.max', function(t) {
+  var result = 9999;
+  var expected = z_index.num(css);
+
+  t.equal(result, expected);
+
+  t.enc();
+});
+
+test('z_index.min', function(t) {
+  var result = -3;
+  var expected = z_index.num(css);
+
+  t.equal(result, expected);
+
+  t.enc();
+})
+
