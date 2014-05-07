0df4755026f2d709caf2caf57220ecfafce13837
Masaaki Morishita <ikasama3.0@gmail.com>
2014-05-08 01:57:49 +0900

    implement .num, .max& .min methods

--- index.js
+++ index.js
@@ -1,16 +1,56 @@
 
 var parse = require('css-parse');
+var _ = require('underscore');
 
 module.exports.num = function(css) {
+  var ast = parse(css);
+  var retNum = 0;
 
+  ast.stylesheet.rules.forEach(function visit(rule) {
+    if (rule.rules) rule.rules.forEach(visit);
+
+    rule.declarations.forEach(function(declaration) {
+      if (declaration.property === 'z-index') {
+        retNum++;
+      }
+    });
+  });
+
+  return retNum;
 };
 
 module.exports.max = function(css) {
+  var ast = parse(css);
+  var z_values = [];
 
+  ast.stylesheet.rules.forEach(function visit(rule) {
+    if (rule.rules) rule.rules.forEach(visit);
+
+    rule.declarations.forEach(function(declaration) {
+      if (declaration.property === 'z-index') {
+        z_values.push(+declaration.value);
+      }
+    });
+  });
+
+  return _.max(z_values);
 };
 
 module.exports.min = function(css) {
+  var ast = parse(css);
+  var z_values = [];
 
+  ast.stylesheet.rules.forEach(function visit(rule) {
+    if (rule.rules) rule.rules.forEach(visit);
+
+    rule.declarations.forEach(function(declaration) {
+      if (declaration.property === 'z-index') {
+        z_values.push(+declaration.value);
+      }
+    });
+  });
+
+  return _.min(z_values);
 };
 
 module.exports.stats = function(css) {
